98d224ae12f4c96e17fc864917dc4330c8a0c8dc
PerraJ60 <perrajgm@gmail.com>
2020-12-15 21:04:30 +0100

    adding Nexus files

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,10 @@ export default function Home() {
   return (
     <div>
       <Layout>
-        <h1>Cool</h1>
+        <h5>
+          This is an app developed in React, NextJS (Create-Next-App),\n using
+          GraphQL, Apollo and Nexus.\n Typescript is also used, as well as Yarn.
+        </h5>
       </Layout>
     </div>
   );
